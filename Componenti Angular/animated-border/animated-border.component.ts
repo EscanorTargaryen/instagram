c8c3914268ce9ac93f6fce80ef51157610fcd6bf
Escanor Targaryen <57372280+EscanorTargaryen@users.noreply.github.com>
2024-02-22 20:55:46 +0100

    Aggingi componente animated border

---
+++ Componenti Angular/animated-border/animated-border.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-border',
+  standalone: true,
+  imports: [],
+  templateUrl: './animated-border.component.html'
+})
+export class AnimatedBorderComponent {
+
+}
